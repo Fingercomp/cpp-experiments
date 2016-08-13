9dc238c55f32f76e8d3928f5dfdcc84b1cc4d8c1
Fingercomp <fingercomp@users.noreply.github.com>
2016-08-13 21:43:06 +0700

    Add an awesome ball texture

--- sfml-hi-world/src/main.cpp
+++ sfml-hi-world/src/main.cpp
@@ -12,7 +12,7 @@ int main() {
                 window.close();
             }
         }
-        window.clear();
+        window.clear(sf::Color::Black);
         window.draw(shape);
         window.display();
     }
