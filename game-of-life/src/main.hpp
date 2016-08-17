9bd1d2ca116932d28a537df314444d54a043c6f1
Fingercomp <fingercomp@users.noreply.github.com>
2016-08-18 02:11:04 +0700

    Move to a separate repository

--- game-of-life/src/main.hpp
+++
@@ -1,4 +0,0 @@
-enum class State {
-    PAUSED,
-    RUNNING
-};
--- game-of-life/src/utils.hpp
+++
@@ -1,8 +0,0 @@
-#include <vector>
-
-
-template <typename T>
-inline void fill(std::vector<T> &vector, int size) {
-    vector.reserve(size);
-    vector.resize(size);
-}
